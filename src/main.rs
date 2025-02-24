8d4aca50e2abb3ceebb7f88b3d5f1ef4c0875a76
山田健太 <knt.yamada.800@gmail.com>
2025-02-24 19:45:06 +0900

    Add sql library

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
 mod api;
 mod entity;
 
-use crate::api::users::{
-    signin::signin,
-    view::view,
-};
 use actix_web::{guard, web, App, HttpServer};
+use api::users::{signin::signin, view::view};
+use dotenv::dotenv;
+use entity::app_state::AppState;
+use sqlx::postgres::PgPoolOptions;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+
+    let url = env::var("DATABASE_URL").expect("[Fatal] DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&url)
+        .await
+        .expect("[Fatal] Failed build connection pools.");
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(AppState { db: pool.clone() }))
             .service(
                 web::scope("/api")
                     .guard(guard::Header("content-type", "application/json"))
                     .route("/users/{id}", web::get().to(view))
-                    .route("/users/signin", web::post().to(signin))
+                    .route("/users/signin", web::post().to(signin)),
             )
     })
     .bind(("127.0.0.1", 8080))?
