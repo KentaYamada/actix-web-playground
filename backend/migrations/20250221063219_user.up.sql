0d36121e8239e83ab4b796b4130f7302d80a0c1f
山田健太 <knt.yamada.800@gmail.com>
2025-02-27 19:37:35 +0900

    Move rust sources

--- backend/migrations/20250221063219_user.up.sql
+++ backend/migrations/20250221063219_user.up.sql
@@ -2,5 +2,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     first_name varchar(20) NOT NULL,
-    family_name varchar(20) NOT NULL
+    family_name varchar(20) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL
 );
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,24 +4,24 @@ mod entity;
 use actix_web::{guard, web, App, HttpServer};
 use api::users::{signin::signin, view::view};
 use dotenv::dotenv;
-use entity::app_state::AppState;
-use sqlx::postgres::PgPoolOptions;
-use std::env;
+// use entity::app_state::AppState;
+// use sqlx::postgres::PgPoolOptions;
+// use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    let url = env::var("DATABASE_URL").expect("[Fatal] DATABASE_URL must be set");
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&url)
-        .await
-        .expect("[Fatal] Failed build connection pools.");
+    // let url = env::var("DATABASE_URL").expect("[Fatal] DATABASE_URL must be set");
+    // let pool = PgPoolOptions::new()
+    //     .max_connections(5)
+    //     .connect(&url)
+    //     .await
+    //     .expect("[Fatal] Failed build connection pools.");
 
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(AppState { db: pool.clone() }))
+            // .app_data(web::Data::new(AppState { db: pool.clone() }))
             .service(
                 web::scope("/api")
                     .guard(guard::Header("content-type", "application/json"))
