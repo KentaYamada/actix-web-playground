da97fab59dcc9182e990022c109ffab18b731cde
山田健太 <knt.yamada.800@gmail.com>
2025-03-02 22:41:25 +0900

    API request仮実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useCallback, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const handleRequestGetTodo = useCallback(() => {
+    axios
+      .get("/api/todos/2", {
+        headers: { "Content-Type": "application/json" },
+        data: {},
+      })
+      .then(function (res) {
+        console.log(res);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }, []);
 
   return (
     <>
@@ -24,12 +39,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <button onClick={handleRequestGetTodo}>Request get todo</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
