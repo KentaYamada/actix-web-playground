e6bbcbd3a586a0769f169a4c47342c0f48950b2a
山田健太 <knt.yamada.800@gmail.com>
2025-03-07 13:20:37 +0900

    Modify eslint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,8 +1,10 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
 import eslintPrettierRecommended from "eslint-plugin-prettier/recommended";
 import vitest from "@vitest/eslint-plugin";
 
@@ -14,18 +16,48 @@ export default tseslint.config(
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
+      complexity: ["error", 10],
       ...reactHooks.configs.recommended.rules,
+      "react/require-default-props": "off",
+      "react/jsx-props-no-spreading": "off",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-key": [
+        "error",
+        {
+          checkFragmentShorthand: true,
+          warnOnDuplicates: true,
+        },
+      ],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
   {
