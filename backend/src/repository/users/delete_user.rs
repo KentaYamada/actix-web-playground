f0c1b51173f9785b2a5cc20d11bf88ccf0d11f0a
山田健太 <knt.yamada.800@gmail.com>
2025-03-18 20:22:47 +0900

    Feat: implement delete user function unittest

--- backend/src/repository/users/delete_user.rs
+++ backend/src/repository/users/delete_user.rs
@@ -6,5 +6,30 @@ pub async fn delete_user(pool: &PgPool, id: i32) -> Result<(), Error> {
         .bind(id)
         .execute(pool)
         .await?;
+
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::entity::user::User;
+    use sqlx::{query_as, PgPool};
+
+    #[sqlx::test(fixtures("users"))]
+    async fn test_delete_user(pool: PgPool) {
+        let user_id = 1;
+
+        delete_user(&pool, user_id)
+            .await
+            .expect("Failed delete user");
+
+        let user = query_as::<_, User>("SELECT * FROM users WHERE id = $1")
+            .bind(user_id)
+            .fetch_optional(&pool)
+            .await
+            .expect("Failed fetch user");
+
+        assert!(user.is_none());
+    }
 }
