7558404a8d8270b77b3a25abe7c93653e420c49f
山田健太 <knt.yamada.800@gmail.com>
2025-04-07 22:03:15 +0900

    Feat: implement error handling

--- backend/src/api/todos/delete.rs
+++ backend/src/api/todos/delete.rs
@@ -1,13 +1,37 @@
-use crate::entity::{app_state::AppState, response::api_response::ApiResponse};
-use crate::repository::todos::delete_todo::delete_todo;
-use actix_web::{web, Responder, Result};
+use crate::{entity::app_state::AppState, repository::todos::delete_todo::delete_todo};
+use actix_web::{
+    body::BoxBody, error::ErrorInternalServerError, http::header::ContentType, web, HttpResponse,
+    Responder, Result,
+};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct DeleteTodoResponse {
+    pub message: &'static str,
+}
+
+impl Responder for DeleteTodoResponse {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &actix_web::HttpRequest) -> actix_web::HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
 
 pub type DeleteTodoPath = web::Path<i32>;
 
-pub async fn delete(state: web::Data<AppState>, path: DeleteTodoPath) -> Result<impl Responder> {
-    delete_todo(&state.db, path.into_inner()).await.unwrap();
-
-    let response = ApiResponse::<()>::new(Some("deleted successfuly".to_string()), None);
-
-    Ok(response.to_json())
+pub async fn delete(
+    state: web::Data<AppState>,
+    path: DeleteTodoPath,
+) -> Result<impl Responder, actix_web::Error> {
+    match delete_todo(&state.db, path.into_inner()).await {
+        Ok(_) => Ok(DeleteTodoResponse {
+            message: "Deleted successfuly",
+        }),
+        Err(_) => Err(ErrorInternalServerError("InternalServerError")),
+    }
 }
