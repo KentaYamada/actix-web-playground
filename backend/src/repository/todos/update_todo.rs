64c6c10eab0db6045e6b9d5ca5cf22c3a0f5d9be
山田健太 <knt.yamada.800@gmail.com>
2025-04-07 22:15:13 +0900

    Feat: implement update todo

--- backend/src/repository/todos/update_todo.rs
+++ backend/src/repository/todos/update_todo.rs
@@ -16,3 +16,31 @@ pub async fn update_todo(pool: &PgPool, todo: &Todo) -> SqlxResult<i32> {
 
     Ok(id)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::entity::todo::Todo;
+    use sqlx::{query_as, PgPool};
+
+    #[sqlx::test(fixtures("todo_fixture"))]
+    async fn test_update_todo_succeeded(pool: PgPool) {
+        let todo_id = 1;
+        let todo = Todo::new(todo_id, 1, "Test", "updated");
+        let updated_id = update_todo(&pool, &todo)
+            .await
+            .expect("Failed to update todo");
+
+        assert_eq!(todo_id, updated_id);
+
+        let updated_todo = query_as::<_, Todo>("SELECT * FROM todos WHERE id = $1")
+            .bind(todo_id)
+            .fetch_one(&pool)
+            .await
+            .expect("Failed to fetch todo");
+
+        assert_eq!(todo.status, updated_todo.status);
+        assert_eq!(todo.title, updated_todo.title);
+        assert_eq!(todo.detail, updated_todo.detail);
+    }
+}
