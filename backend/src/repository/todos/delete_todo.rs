8f8fc68d61db5b0b4158b548d6ed8eb883d0fbea
山田健太 <knt.yamada.800@gmail.com>
2025-04-07 11:29:50 +0900

    Feat: implement delete todo unittest

--- backend/src/repository/todos/delete_todo.rs
+++ backend/src/repository/todos/delete_todo.rs
@@ -9,3 +9,26 @@ pub async fn delete_todo(pool: &PgPool, id: i32) -> SqlxResult<()> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::entity::todo::Todo;
+    use sqlx::{query_as, PgPool};
+
+    #[sqlx::test(fixtures("todo_fixture"))]
+    async fn test_delete_todo(pool: PgPool) {
+        let todo_id = 1;
+        delete_todo(&pool, todo_id)
+            .await
+            .expect("Failed to delete todo");
+
+        let todo = query_as::<_, Todo>("SELECT * FROM todos WHERE id = $1")
+            .bind(todo_id)
+            .fetch_optional(&pool)
+            .await
+            .expect("Failed to fetch todo");
+
+        assert!(todo.is_none());
+    }
+}
