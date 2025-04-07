3e8e535ecbc9e19bab690441fad7d3ec43d14c27
山田健太 <knt.yamada.800@gmail.com>
2025-04-07 22:10:41 +0900

    Feat: implement error handling

--- backend/src/api/todos/view.rs
+++ backend/src/api/todos/view.rs
@@ -1,20 +1,42 @@
-use crate::entity::{app_state::AppState, response::api_response::ApiResponse, todo::Todo};
-use crate::repository::todos::fetch_todo_by_id::fetch_todo_by_id;
-use actix_web::{web, Responder, Result};
+use crate::{
+    entity::{app_state::AppState, todo::Todo},
+    repository::todos::fetch_todo_by_id::fetch_todo_by_id,
+};
+use actix_web::{
+    body::BoxBody,
+    error::{ErrorInternalServerError, ErrorNotFound},
+    http::header::ContentType,
+    web, HttpResponse, Responder, Result,
+};
 use serde::Serialize;
 
 #[derive(Serialize)]
-pub struct TodoViewResponseBody {
+pub struct TodoViewResponse {
     pub todo: Todo,
+}
+
+impl Responder for TodoViewResponse {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &actix_web::HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
 }
 
 pub type TodoViewRequestPath = web::Path<i32>;
 
 pub async fn view(state: web::Data<AppState>, path: TodoViewRequestPath) -> Result<impl Responder> {
-    let todo: Todo = fetch_todo_by_id(&state.db, path.into_inner())
-        .await
-        .unwrap();
-    let response = ApiResponse::new(None, Some(TodoViewResponseBody { todo }));
-
-    Ok(response.to_json())
+    match fetch_todo_by_id(&state.db, path.into_inner()).await {
+        Ok(Some(todo)) => Ok(TodoViewResponse { todo }),
+        Ok(None) => Err(ErrorNotFound(
+            serde_json::json!({ "message": "User not found." }),
+        )),
+        Err(_) => Err(ErrorInternalServerError(
+            serde_json::json!({ "message": "InternalServerError"}),
+        )),
+    }
 }
