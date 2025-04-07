e64e54628cefb7ddc2739a4139790218c583c3cc
山田健太 <knt.yamada.800@gmail.com>
2025-04-07 22:14:34 +0900

    Feat: implement error handling

--- backend/src/api/todos/list.rs
+++ backend/src/api/todos/list.rs
@@ -1,16 +1,35 @@
-use crate::entity::{app_state::AppState, response::api_response::ApiResponse, todo::Todo};
-use crate::repository::todos::fetch_todos::fetch_todos;
-use actix_web::{web, Responder, Result};
+use crate::{
+    entity::{app_state::AppState, todo::Todo},
+    repository::todos::fetch_todos::fetch_todos,
+};
+use actix_web::{
+    body::BoxBody, error::ErrorInternalServerError, http::header::ContentType, web, HttpResponse,
+    Responder, Result,
+};
 use serde::Serialize;
 
 #[derive(Serialize)]
-pub struct TodoListResponseBody {
+pub struct TodoListResponse {
     pub todos: Vec<Todo>,
 }
 
-pub async fn list(state: web::Data<AppState>) -> Result<impl Responder> {
-    let todos: Vec<Todo> = fetch_todos(&state.db).await.unwrap();
-    let response = ApiResponse::new(None, Some(TodoListResponseBody { todos }));
+impl Responder for TodoListResponse {
+    type Body = BoxBody;
 
-    Ok(response.to_json())
+    fn respond_to(self, _req: &actix_web::HttpRequest) -> actix_web::HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
+
+pub async fn list(state: web::Data<AppState>) -> Result<impl Responder, actix_web::Error> {
+    match fetch_todos(&state.db).await {
+        Ok(todos) => Ok(TodoListResponse { todos }),
+        Err(_) => Err(ErrorInternalServerError(
+            serde_json::json!({ "message": "InternalServerError" }),
+        )),
+    }
 }
