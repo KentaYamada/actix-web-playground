109cf412941ef5f634384f4d7c27df5024457b0c
山田健太 <knt.yamada.800@gmail.com>
2025-04-10 22:10:57 +0900

    Feat: TodoForm UI layout

--- frontend/src/pages/todos/TodoEditPage.tsx
+++ frontend/src/pages/todos/TodoEditPage.tsx
@@ -1,3 +1,19 @@
+import { Link } from "react-router";
+import { Breadcrumbs, Divider, Title } from "@mantine/core";
+import { DefaultLayout, TodoForm } from "@components";
+
 export function TodoEditPage() {
-  return <h2>Todo Edit Page</h2>;
+  return (
+    <DefaultLayout>
+      <Breadcrumbs separator=">">
+        <Link to="/todos">ToDo一覧</Link>
+        <span>ToDo編集</span>
+      </Breadcrumbs>
+      <Title order={2} mt="md">
+        ToDo編集
+      </Title>
+      <Divider my="md" />
+      <TodoForm />
+    </DefaultLayout>
+  );
 }
