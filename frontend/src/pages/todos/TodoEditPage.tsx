be88bdbe8b256c6bf1ce1b777be260f4bb32e5d6
山田健太 <knt.yamada.800@gmail.com>
2025-04-14 22:26:04 +0900

    Feat: implement edit api

--- frontend/src/pages/todos/TodoEditPage.tsx
+++ frontend/src/pages/todos/TodoEditPage.tsx
@@ -1,23 +1,53 @@
 import { useEffect } from "react";
-import { Link } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import { Breadcrumbs, Divider, Title } from "@mantine/core";
 import { DefaultLayout, TodoForm } from "@components";
-import { useTodoForm, TodoFormValuesType } from "@hooks";
+import { ApiResponseError, Todo } from "@entity";
+import { useTodoApi, useTodoForm, TodoFormValuesType } from "@hooks";
 import { ErrorMessage, useErrorMessage } from "@shared/ui";
 
 export function TodoEditPage() {
+  const navigate = useNavigate();
+  const { id } = useParams();
   const {
     errorMessageConfig,
     visibleErrorMessage,
     showErrorMessage,
     hideErrorMessage,
   } = useErrorMessage();
-  const { form, invalid } = useTodoForm();
+  const { getTodoApi, updateTodoApi } = useTodoApi();
+  const { form, invalid, updateFormValues } = useTodoForm();
 
   const handleSave = form.onSubmit((formValues: TodoFormValuesType) => {
+    const payload: Todo = { ...formValues };
+
     hideErrorMessage();
-    console.log(formValues);
+    updateTodoApi(payload)
+      .then(() => navigate("/todos"))
+      .catch((reason: ApiResponseError) => {
+        showErrorMessage({
+          title: "システムエラー",
+          message: reason.message,
+        });
+      });
   });
+
+  useEffect(() => {
+    if (id !== undefined && id !== "") {
+      getTodoApi(Number(id))
+        .then((res) => updateFormValues(res.data.todo))
+        .catch((reason: ApiResponseError) => {
+          if (reason.status === 404) {
+            navigate("/notfound");
+          }
+
+          showErrorMessage({
+            title: "システムエラー",
+            message: reason.message,
+          });
+        });
+    }
+  }, [id, navigate, updateFormValues, showErrorMessage, getTodoApi]);
 
   useEffect(() => {
     if (invalid) {
